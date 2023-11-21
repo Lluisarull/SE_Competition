b8e2aa7f178019215f651b52119aaaf9e90c45cc
4Freye <4freye@gmail.com>
2023-11-20 19:59:47 -0500

    updated processing file to output train and test

--- src/data_processing.py
+++ src/data_processing.py
@@ -46,7 +46,6 @@ def process_load_data(load_data):
 
     return load_data_full
     
-
 def process_gen_data(gen_data):
 
     gen_data['Time'] = pd.to_datetime(gen_data.Time)
@@ -117,6 +116,179 @@ def save_data(data, filepath):
     data.to_csv(filepath)
     pass
 
+def process_load_2(load_data):
+    # datetime object
+    load_data['Time'] = pd.to_datetime(load_data['Time'])
+
+    # Drop unnecessary columns
+    load_data.drop(['AreaID', 'UnitName','PsrType'], axis=1, inplace=True)
+
+    # Hour aggregation
+    load_data = hour_agg(load_data, ['CountryID'], 'Time', 'quantity')
+
+    # Create a datetime series with hourly frequency from the minimum to maximum 'Time'
+    datetime_series = pd.date_range(start=load_data['Time'].min(), end=load_data['Time'].max(), freq='H')
+
+    # Create a range of unique 'CountryID' values
+    numbers_range = load_data['CountryID'].unique()
+
+    # Create a MultiIndex from the Cartesian product of 'datetime_series' and 'numbers_range'
+    index = pd.MultiIndex.from_product([datetime_series, numbers_range], names=['Time', 'CountryID'])
+
+    # Create a DataFrame with the MultiIndex
+    df_index = pd.DataFrame(index=index).reset_index()
+
+    # Merge the created DataFrame with the MultiIndex with the original 'load_data'
+    load_data_full = df_index.merge(load_data, how='left')
+
+    return load_data_full
+
+def process_gen_2(gen_data):
+    # Datetime object
+    gen_data['Time'] = pd.to_datetime(gen_data.Time)
+    #Drop unnecessary columns
+    gen_data.drop(['AreaID', 'UnitName'], axis=1, inplace=True)
+    
+    datetime_series = pd.date_range(start=gen_data.Time.min(), end=gen_data.Time.max(), freq='H')
+    numbers_range = gen_data.CountryID.unique()
+    psr_vals = gen_data.PsrType.unique()
+
+    # Creating a multi-index from cartesian product of both ranges
+    index = pd.MultiIndex.from_product([datetime_series, numbers_range, psr_vals], names=['Time', 'CountryID', 'PsrType'])
+
+    # Creating a DataFrame with the multi-index
+    df_index = pd.DataFrame(index=index).reset_index()
+
+    gen_data_2 = df_index.merge(gen_data, how='left')
+
+    gen_data_full = gen_data_2.pivot_table(index = ['Time','CountryID'], columns= ['PsrType'], values='quantity')
+    
+    return gen_data_full
+
+def hour_agg(data, groupby_columns, time_column, value_column):
+    """
+    Perform hourly aggregation on the specified DataFrame.
+
+    Parameters:
+    - data: DataFrame to be aggregated.
+    - groupby_columns: List of columns to group by.
+    - time_column: Name of the time column.
+    - value_column: Name of the column to aggregate.
+
+    Returns:
+    - Aggregated DataFrame.
+    """
+    return (
+        data.groupby([*groupby_columns, data[time_column].dt.round('H')], sort=False)
+            .agg({value_column: 'sum'})
+            .reset_index()
+    )
+
+def _fill_missing_dates(df: pd.DataFrame, min_date: pd.Timestamp, max_date: pd.Timestamp) -> pd.DataFrame:
+    """Fill missing dates in the time series between the minimum and maximum dates and set their
+    values to NaN.
+
+    :param df: Time series sales data for a specific country-brand.
+    :param min_date: Minimum date to be considered.
+    :param max_date: Maximum date to be considered.
+    :return: Complete time series for a specific country-type.
+    """
+
+    df['Time'] = pd.to_datetime(df['Time'])
+    complete_date_range = pd.date_range(start=min_date, end=max_date, freq='H')
+    complete_df = (
+        pd.DataFrame({'Time': complete_date_range})
+        .merge(df[['CountryID']].drop_duplicates(), how='cross')
+    )
+    result_df = complete_df.merge(df, on=['Time', 'CountryID'], how='left')
+
+    return result_df
+
+def calculate_green_energy(df):
+    green_energy = ['B01', 'B09', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'B16', 'B17', 'B18', 'B19']
+    df['green_energy'] = df[green_energy].sum(axis=1, skipna=True)
+    df = df[['Time', 'CountryID', 'load', 'green_energy']]
+    return df
+
+
+def pivot_and_flatten(df, index_col, country_col, value_cols, aggfunc='first'):
+    """
+    Pivot the DataFrame from long to wide, flatten multi-level columns, and reset the index.
+
+    Parameters:
+    - df: Input DataFrame.
+    - index_col: Column to be used as the index in the wide DataFrame.
+    - country_col: Column to be used as columns in the wide DataFrame.
+    - value_cols: List of columns to be used as values in the wide DataFrame.
+    - aggfunc: Aggregation function for pivot_table.
+
+    Returns:
+    - Wide DataFrame with flattened columns and reset index.
+    """
+    # Pivot the DataFrame from long to wide
+    wide_df = df.pivot_table(index=index_col, columns=country_col, values=value_cols, aggfunc=aggfunc)
+
+    # Flatten the multi-level columns
+    wide_df.columns = [f'{col}_{country}' for col, country in wide_df.columns]
+
+    # Resetting the index
+    wide_df = wide_df.reset_index()
+
+    return wide_df
+
+def impute_nans(df):
+    """
+    Impute NaN values in a DataFrame with specific logic:
+    - If all rows in a column are missing, set the value to 0.
+    - Otherwise, impute the NaN with the mean between the previous and the following value.
+
+    Parameters:
+    - df: Input DataFrame.
+
+    Returns:
+    - DataFrame with NaN values imputed based on the specified logic.
+    """
+    for col in df.columns:
+        # Check if all rows in the column are missing
+        if df[col].isnull().all():
+            # Set the value to 0 if all rows are missing
+            df[col] = 0
+        elif df[col].dtype == 'datetime64[ns]':
+            # Impute NaN with the mean of the datetime values
+            df[col] = df[col].fillna(df[col].mean())
+        else:
+            # Impute NaN with the mean between the previous and the following value
+            df[col] = df[col].fillna((df[col].shift() + df[col].shift(-1)) / 2)
+            df[col] = df[col].fillna(0)
+
+    return df
+
+def preprocess_data_2(load_data, gen_data):
+    load_data_full = process_load_2(load_data)
+    gen_data_full = process_gen_2(gen_data)
+    data = load_data_full.rename({'quantity':'load'}, axis=1).set_index(['CountryID','Time']).merge(gen_data_full, left_index=True, right_index=True)
+    data = data.reset_index()
+    data_clean = _fill_missing_dates(data,min_date=data['Time'].min(),max_date=data['Time'].max())
+    data_clean_2 = data_clean.groupby(['Time', 'CountryID']).apply(calculate_green_energy)
+    data_clean_2 = data_clean_2.reset_index(drop=True)
+    data_clean_wide = pivot_and_flatten(data_clean_2, index_col='Time', country_col='CountryID', value_cols=['green_energy', 'load'])
+    data_clean_wide_imputed = impute_nans(data_clean_wide)
+    return data_clean_wide_imputed
+
+def split_data(df):
+    # keep sequential order of data
+    df.sort_values(by='Time', inplace = True) 
+    # 80/20 split
+    train_size = int(len(df) * 0.8)
+    #split data
+    train, test = df[:train_size], df[train_size:]
+    return train, test
+
+def save_data_wide(train, test, train_output, test_output):
+    train.to_csv(train_output)
+    test.to_csv(test_output)
+    pass
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Data processing script for Energy Forecasting Hackathon')
     parser.add_argument(
@@ -137,14 +309,29 @@ def parse_arguments():
         default='data/clean/data.csv', 
         help='Path to save the processed train data'
     )
+    parser.add_argument(
+        '--train_output', 
+        type=str,
+        default='data/clean/train.csv', 
+        help='Path to save the processed data in wide format.'
+    )
+    parser.add_argument(
+        '--test_output', 
+        type=str,
+        default='data/clean/train.csv', 
+        help='Path to save the processed data in wide format.'
+    )
     return parser.parse_args()
 
-def main(input_gen_file, input_load_file, output_data_file):
+def main(input_gen_file, input_load_file, output_data_file, train_output, test_output):
     loaded_data, gen_data = load_data('data/master_load.csv', 'data/master_gen.csv')
     data = preprocess_data(loaded_data,gen_data)
     save_data(data, output_data_file)
+    loaded_data_wide, gen_data_wide = load_data(input_load_file, input_gen_file)
+    data_clean_wide_imputed = preprocess_data_2(loaded_data_wide, gen_data_wide)
+    train, test = split_data(data_clean_wide_imputed)
+    save_data_wide(train, test, train_output, test_output)
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.input_gen_file, args.input_load_file, args.output_data_file)
-
+    main(args.input_gen_file, args.input_load_file, args.output_data_file, args.train_output, args.test_output)
